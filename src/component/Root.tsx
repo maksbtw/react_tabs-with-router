f32432bf6f8e1d7633b32006d0a7f89b5548f5e2
Maksym Savchenko <maks.20050817@gmail.com>
2026-07-20 13:30:36 +0200

    ready to check

---
+++ src/component/Root.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { App } from '../App';
+import { TabsPage } from '../pages/TabsPage';
+import { HomePage } from '../pages/HomePage';
+import { NotFoundPage } from '../pages/NotFoundPage';
+
+export const Root = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+
+        <Route path="/tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
+};
---
+++ src/pages/TabsPage.tsx
@@ -0,0 +1,41 @@
+import { Tab } from '../types/Tab';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+
+const tabs: Tab[] = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
+
+export const TabsPage = () => {
+  const { tabId } = useParams();
+
+  const selectedTab = tabId ? tabs.find(tab => tabId === tab.id) : undefined;
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              data-cy="Tab"
+              className={classNames({
+                'is-active': tabId === tab.id,
+              })}
+              key={tab.id}
+            >
+              <Link to={tabId ? `../${tab.id}` : `${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {selectedTab ? selectedTab?.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
